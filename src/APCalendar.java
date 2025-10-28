74571c81ae7ec1e984188c26a2d3f609439b4e33
KelvinCodesInC <kelvino28@nycstudents.net>
2025-10-27 20:43:36 -0400

    Update APCalendar.java

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class APCalendar {
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        APCalendar calendar = new APCalendar();
+        System.out.println(calendar.dayOfWeek(3, 1, 2024)); // March 1, 2024
+    }
 }
